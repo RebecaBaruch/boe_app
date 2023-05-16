927c713434683c8b3f213b36445842da3d021950
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2023-05-16 13:39:04 -0300

    feat: cowDetails.html almost done!!!

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,13 +1,18 @@
+//cores da rosca
 const ok = "#22DF7A";
 const medium = "#FFEB36";
 const dangerous = "#FF5454";
 const levelColor = [ok, medium, dangerous];
 
+//quanto cabe na rosca
+let dataValue = 50;
+let fill = 100 - dataValue;
+
 // Dados de exemplo
 const data = {
     datasets: [{
-      data: [70, 30],
-      backgroundColor: [levelColor[0], "#717171"]
+      data: [fill, dataValue],
+      backgroundColor: ["#717171", levelColor[1]]
     }]
   };
 
